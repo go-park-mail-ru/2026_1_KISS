4bb868a4e64e68f00ac028880a54bda05948620a
khosta77 <puwerfulpants@gmail.com>
2026-04-17 21:43:19 +0300

    feat(runner): add gRPC server, notebook adapter, and service entrypoint
    Implement RunnerServiceServer with ExecuteFromPosition, ExecuteBlock,
    StopSession RPCs. Add NotebookAdapter and BlockAdapter that implement
    repository interfaces via gRPC calls to Notebook Service, keeping
    runner_service.go unchanged. Add cmd/runner entrypoint and bufconn tests.

---
+++ internal/runner/app/app.go
@@ -0,0 +1,94 @@
+package app
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"github.com/go-park-mail-ru/2026_1_KISS/internal/pkg/config"
+	"github.com/go-park-mail-ru/2026_1_KISS/internal/pkg/grpcutil"
+	"github.com/go-park-mail-ru/2026_1_KISS/internal/runner/container"
+	runnergrpc "github.com/go-park-mail-ru/2026_1_KISS/internal/runner/grpc"
+	"github.com/go-park-mail-ru/2026_1_KISS/internal/runner/runner_service"
+	"github.com/go-park-mail-ru/2026_1_KISS/internal/runner/session_repository"
+	pbnotebook "github.com/go-park-mail-ru/2026_1_KISS/pkg/api/notebook"
+	pbrunner "github.com/go-park-mail-ru/2026_1_KISS/pkg/api/runner"
+)
+
+type App struct {
+	grpcServer    *grpc.Server
+	listener      net.Listener
+	runnerManager container.Manager
+	cancelReaper  context.CancelFunc
+	nbConn        *grpc.ClientConn
+}
+
+func New(cfg *config.Config, grpcPort string) (*App, error) {
+	nbConn, err := grpc.NewClient(cfg.GRPC.NotebookAddr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("connect notebook service: %w", err)
+	}
+	nbClient := pbnotebook.NewNotebookServiceClient(nbConn)
+
+	notebookAdapter := runnergrpc.NewNotebookAdapter(nbClient)
+	blockAdapter := runnergrpc.NewBlockAdapter(nbClient)
+
+	runnerManager, err := container.NewManager(cfg.Runner)
+	if err != nil {
+		nbConn.Close()
+		return nil, fmt.Errorf("init runner manager: %w", err)
+	}
+
+	execSessionRepo := session_repository.NewExecutionSessionRepository(cfg.Runner.ExecutionTimeout)
+	runnerSvc := runner_service.NewRunnerService(runnerManager, execSessionRepo, notebookAdapter, blockAdapter, cfg.Runner.IdleTimeout)
+
+	reaperCtx, cancelReaper := context.WithCancel(context.Background())
+	go runnerSvc.StartIdleReaper(reaperCtx)
+
+	lis, err := net.Listen("tcp", ":"+grpcPort)
+	if err != nil {
+		cancelReaper()
+		nbConn.Close()
+		return nil, fmt.Errorf("listen: %w", err)
+	}
+
+	srv := grpc.NewServer(
+		grpc.ChainUnaryInterceptor(
+			grpcutil.RecoveryUnaryInterceptor(),
+			grpcutil.LoggingUnaryInterceptor(),
+		),
+	)
+	pbrunner.RegisterRunnerServiceServer(srv, runnergrpc.NewServer(runnerSvc))
+
+	return &App{
+		grpcServer:    srv,
+		listener:      lis,
+		runnerManager: runnerManager,
+		cancelReaper:  cancelReaper,
+		nbConn:        nbConn,
+	}, nil
+}
+
+func (a *App) Run() error {
+	slog.Info("runner service started", "addr", a.listener.Addr().String())
+	return a.grpcServer.Serve(a.listener)
+}
+
+func (a *App) Shutdown(_ context.Context) {
+	a.cancelReaper()
+	a.grpcServer.GracefulStop()
+	if a.runnerManager != nil {
+		if err := a.runnerManager.Close(); err != nil {
+			slog.Error("runner manager close error", "error", err)
+		}
+	}
+	if a.nbConn != nil {
+		a.nbConn.Close()
+	}
+}
