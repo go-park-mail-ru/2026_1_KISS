6b9d6821323a4876455c638c817d0ed257397e9e
khosta77 <puwerfulpants@gmail.com>
2026-04-18 18:02:42 +0300

    test(infra): remove TestConnect_Success that only tested sqlmock
    The test exercised sqlmock.New(), not the actual Connect() function.
    Real coverage comes from TestConnect_InvalidDSN and TestConnect_InvalidDriver.

--- internal/pkg/database/postgres_test.go
+++ internal/pkg/database/postgres_test.go
@@ -2,16 +2,7 @@ package database
 
 import (
 	"testing"
-
-	"github.com/DATA-DOG/go-sqlmock"
 )
-
-func TestConnect_Success(t *testing.T) {
-	_, _, err := sqlmock.New(sqlmock.MonitorPingsOption(true))
-	if err != nil {
-		t.Fatalf("failed to create mock db: %v", err)
-	}
-}
 
 func TestConnect_InvalidDSN(t *testing.T) {
 	_, err := Connect("")
